6012f1d7882bbf9234eee26af2927fc378e2cee1
Lambda_School_Loaner_206 <constancebeebe@gmail.com>
2019-11-12 20:31:00 -0500

    MVP met

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+  
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   </div>
 );
 
